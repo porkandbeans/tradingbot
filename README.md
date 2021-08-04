a34a54cefdec7425604e3da60144bdfeeeb39bdf
Charlie <voter96@gmail.com>
2021-08-04 14:31:59 +1200

    bp.tf data to JSON is now a thing

--- README.md
+++ README.md
@@ -2,10 +2,8 @@ interact with my bot at https://steamcommunity.com/id/gokritz_bot
 
 This is the source code for my trading bot that I'm putting up on github for potential employers, or curious individuals.
 
-This requires node.js and some packages from the repo
-if you're not using linux, you're gonna have a bad time
+This requires node.js and some packages from the repo.
 ```
-sudo pacman -Syu nodejs npm || sudo apt-get update && sudo apt-get install nodejs npm
 npm install steam-user steam-totp backpacktf
 ```
 
--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const SteamUser = require('steam-user'); // used for interfacing with steam
 const SteamTotp = require('steam-totp'); // authenticating with steam-guard
-const Bptf = require('backpacktf'); // check prices on backpack.tf
+const bptf = require('backpacktf'); // check prices on backpack.tf
 const fs = require("fs"); // writing logs
 const d = new Date(); // also for writing logs, pretty much.
 
@@ -38,11 +38,11 @@ client.on('loggedOn', () => {
     client.setPersona(SteamUser.EPersonaState.Online); // set status online
     client.gamesPlayed(440) // set status to playing TF2
 
-    bptfData = Bptf.getCommunityPrices(config['bptf-api'], "440", (err, data) => {
+    /*bptfData = Bptf.getCommunityPrices(config['bptf-api'], "440", (err, data) => {
         if (err) {
             console.log("Error: " + err.message);
         }
-    })
+    })*/
 });
 
 /* whenever a relationship with a friend changes
@@ -78,6 +78,8 @@ client.on('friendOrChatMessage', (senderID, receivedMessage, room) => {
         return;
     }
 
+    // if the user asks to be rated, the bot will berate or compliment them based on a 50/50 dice roll and pull responses from responses.json
+    // no, this doesn't need to exist, well spotted.
     if (receivedMessage == "!rateme") {
         d12 = Math.floor(Math.random() * 12);
         if (d12 >= 6) {
@@ -93,27 +95,35 @@ client.on('friendOrChatMessage', (senderID, receivedMessage, room) => {
         return;
     }
 
+    // when called, this gets a shitload (224,152 lines) of data from backpack.tf
+    // and stores it in BPTF.json
+    // if BPTF.json already exists, it overwrites
+    // (hence being !update and not !get)
+    // I also made it only listen for this command from me, since
+    // it could be abused to spam the API or guzzle up my memory
+    if(receivedMessage == "!update"){
+        if(senderID == config['my-id']){
+            fs.writeFile("BPTF.json", "", (err) =>{
+                if (err) throw err;
+                console.log("Nullified BPTF.json");
+            })
 
-    if (receivedMessage.startsWith("!getprice ")) {
-        //receives user input and replies with the price of an item according to backpack.tf
-        item = receivedMessage.substring("!getprice ".length, receivedMessage.length);
-        if (item != null || item != "") {
-            // TODO this
-            sendMessage(senderID, "I am now searching for " + item);
-        } else {
-            sendMessage(senderID, "Enter \"!getprice\" followed by the name of the item you want pricechecked.");
+            bptf.getCommunityPrices(
+                config['bptf-api'],
+                "440",
+                (err, data) => {
+                    if(err) throw err;
+                    fs.writeFile(
+                        "BPTF.json",
+                        JSON.stringify(data, null, 1),
+                        (err) => {
+                            if (err) throw err;
+                            console.log("file written");
+                        })
+                })
+            sendMessage(senderID, "updated my prices.");
+        }else{
+            sendMessage(senderID, "You are not my master!");
         }
-        return;
     }
-
-    if (receivedMessage == "!getprice") {
-        sendMessage(
-            senderID,
-            "Enter \"!getprice\" followed by the name of the item you want pricechecked.");
-        sendMessage(
-            senderID,
-            "For example: \"!getprice key\", \"!getprice towering pillar of hats\"");
-        return;
-    }
-
 });
